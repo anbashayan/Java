2b68102e34c5f0497adb77084e2d259fd4c7ffc1
anbashayan <anbashayan12@gmail.com>
2025-07-21 15:17:41 +0530

    logic

---
+++ logic.java
@@ -0,0 +1,12 @@
+public class logic {
+    public static void main(String args[]){
+        boolean cricket = true;
+        boolean football = false;
+
+        if(cricket || football){
+            System.out.println("Play");
+        }else{
+            System.out.println("Don't play");
+        }
+    }
+}
